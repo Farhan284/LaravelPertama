172ecebf2d4b3c40cdd82a03e059b6a95da81947
Farh4nn <541231070@student.smktelkom-pwt.sch.id>
2024-10-16 14:15:49 +0700

    add index and login

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //meruju ke file welcome, blade, php di folder resource
 });
+
+Route::get('/login', function () {
+    return view('loginindex'); //meruju ke file welcome, blade, php di folder resource
+});
+
+Route::get('/homepage', function () {
+    return view('index'); //meruju ke file welcome, blade, php di folder resource
+});
+
+
